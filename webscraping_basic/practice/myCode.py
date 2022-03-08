32ff9aa328e12880ec3264f712ef755d559bc63b
KDY <kimdoyoung@gmail.com>
2022-03-08 23:56:10 +0900

    commit

--- webscraping_basic/practice/myCode.py
+++ webscraping_basic/practice/myCode.py
@@ -55,3 +55,10 @@
 # print(lst)
 
 """ 5 """
+import requests
+url = "https://nadocoding.tistory.com"
+headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36"}
+res = requests.get(url, headers=headers)
+res.raise_for_status()
+with open("nadocoding.html", "w", encoding="utf8") as f:  
+    f.write(res.text)
