325e7e538e6220faefa619b748fe856f0040e2bd
KDY <kimdoyoung@gmail.com>
2022-03-14 18:42:22 +0900

    commit

--- webscraping_basic/practice/myCode.py
+++ webscraping_basic/practice/myCode.py
@@ -319,3 +319,37 @@
 #         # print(data)
 #         writer.writerow(data)
 
+# """ 13 """
+# from concurrent.futures import BrokenExecutor
+# from selenium import webdriver
+# import time
+
+# browser = webdriver.Chrome("/Users/KDY/Workspace/python/python-from-scratch/webscraping_basic/practice/chromedriver")
+
+# # 1. 네이버 이동
+# browser.get("https://www.naver.com/")
+
+# # 2. 로그인 버튼 클릭
+# elem = browser.find_element_by_class_name("link_login")
+# elem.click()
+
+# # 3. id, pw 입력
+# browser.find_element_by_id("id").send_keys("naver_id")
+# browser.find_element_by_id("pw").send_keys("password")
+
+# # 4. 로그인 버튼 클릭
+# browser.find_element_by_id("log.login").click()
+# time.sleep(1)
+
+# # 5. id 새로 입력
+# browser.find_element_by_id("id").clear()
+# browser.find_element_by_id("id").send_keys("my_id")
+# browser.find_element_by_id("pw").send_keys("password")
+
+# # 6. html 정보 출력
+# print(browser.page_source)
+
+# # 7. 브라우저 종료
+# browser.quit()
+
+""" 14 """
